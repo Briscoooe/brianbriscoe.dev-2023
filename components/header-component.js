203bdc2c4c7209312e4cbad2b73e0eb623a553cb
Briscoooe <brianbriscoe06@gmail.com>
2022-10-08 15:44:11 +0100

    init

---
+++ components/header-component.js
@@ -0,0 +1,22 @@
+class HeaderComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+          <a href="/" class="title">
+              Brian Briscoe
+          </a>
+          <nav>
+              <a href="index.html">Home</a>
+              <a href="https://twitter.com/brianbriscoe_">Twitter</a>
+              <a href="blog.html">Blog</a>
+          </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define('header-component', HeaderComponent);
