f9b94949ea4ced322a2bd2e3ae662af9c4eb80ba
Briscoooe <brianbriscoe06@gmail.com>
2023-05-31 18:49:56 +0100

    hcat stuff

--- components/header-component.js
+++ components/header-component.js
@@ -12,8 +12,10 @@ class HeaderComponent extends HTMLElement {
           <nav>
               <a href="/">Home</a>
               <a href="https://twitter.com/brianbriscoe_">Twitter</a>
-              <a href="/blog">Blog</a>
-              <a href="/resume">Resume</a>
+              <a href="/resume" style="display: flex;flex-direction: row;align-items: center">
+                Resume
+                <img src="../assets/magic.svg" alt="magic" height="24" width="24" style="margin-left: 10px"/>
+              </a>
           </nav>
       </header>
     `;
