396c03dbf19f26820b8395ec4848f371d6fd2dfd
Briscoooe <brianbriscoe06@gmail.com>
2024-05-16 22:30:05 +0100

    update 2024

--- components/header-component.js
+++ components/header-component.js
@@ -12,12 +12,13 @@ class HeaderComponent extends HTMLElement {
           </a>
           <nav>
               <a href="/">Home</a>
+              <a href="/blog">Blog</a>
               <a href="https://twitter.com/brianbriscoe_">Twitter</a>
               <a href="https://github.com/briscoooe">GitHub</a>
-              <a href="/resume" style="display: flex;flex-direction: row;align-items: center">
-                ResumAI
-                <img src="../assets/magic.svg" alt="magic" height="24" width="24" style="margin-left: 4px"/>
-              </a>
+<!--              <a href="/resume" style="display: flex;flex-direction: row;align-items: center">-->
+<!--                ResumAI-->
+<!--                <img src="../assets/magic.svg" alt="magic" height="24" width="24" style="margin-left: 4px"/>-->
+<!--              </a>-->
           </nav>
       </header>
     `;
